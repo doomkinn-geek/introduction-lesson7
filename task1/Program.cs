fdc45049ef0cf20c93c38b2641b1bb8d3ca5af89
doomkinn-geek <doomkinn@yandex.ru>
2021-04-12 16:22:10 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Введите а: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a % b;
+            Console.WriteLine($"Результат '%' - {c}");
+            c = a / b;
+            Console.WriteLine($"Результат '/' - {c}");
+            c = a * b;
+            Console.WriteLine($"Результат '*' - {c}");
+            c = a - b;
+            Console.WriteLine($"Результат '-' - {c}");
+            c = a + b;
+            Console.WriteLine($"Результат '+' - {c}");
+        }
+    }
+}
